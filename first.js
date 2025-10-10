3c737b25e5b510e97d06fe74b5690f427333614b
Md Piash Islam <113939505+piash25252@users.noreply.github.com>
2025-10-11 02:04:27 +0600

    Initial commit

---
+++ first.js
@@ -0,0 +1,26 @@
+
+// // console.log("Hello batch 13.\nI am passoniate learner sqa manual and automation.");
+// // console.log(100);
+
+// function first()
+// {
+// const data_one = "Hello batch 13.\n";
+// const data_two = 100;
+// const data_three = "\nThis is new line"
+// const data_four = ["piash",43,"DIU"]
+// const data_five = {name:"Piash", designation:"student"};
+// const data_six = 10
+// // console.log(data_one,data_two,data_three,data_four,data_five);
+// // console.log(typeof(data_five));
+// console.log(data_six+data_two);
+// }
+// first()
+
+
+
+function first()
+{
+    const data_four = ["test",13,"batch"];
+    console.log(data_four[data_four.length-1]);
+}
+first();
---
+++ loop.js
@@ -0,0 +1,35 @@
+
+// // for(let i=0;i<=5;i++)
+// // {
+// //     console.log(i);
+// // }
+
+// const fruits = ["apple","mango","lici"]
+
+// for(let i =0;i<fruits.length;i++)
+// {
+//     console.log(fruits[i])
+// }
+
+
+// let fruits = ["apple","mango","lici"]
+// //  console.log(fruits.length)
+// fruits.reverse();
+// console.log(fruits)
+
+// let age = prompt("Tomar boyosh likho:");
+
+// age = Number(age); // string ke number e convert korlam
+
+// if (age >= 18) {
+//   console.log("You are adult");
+// } else {
+//   console.log("You are minor");
+// }
+// node app.js 20
+
+// let x = 5;
+// let y = 10;
+
+// let z = x+y;
+// console.log(z)
