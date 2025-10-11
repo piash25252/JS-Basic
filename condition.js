85135b6dc1e09b89e9cfc2eb997732ddaf54ea2c
Md Piash Islam <113939505+piash25252@users.noreply.github.com>
2025-10-12 03:25:46 +0600

    Added condition.js and for_loop.js

---
+++ condition.js
@@ -0,0 +1,24 @@
+
+function condition()
+{
+    const data = 20;
+    if(data == 0)
+    {
+        console.log(data+ " is a even number");
+    }
+    else if (data % 2 == 1 && data > 0)
+    {
+         console.log(data+ " is a odd number");
+    }
+    else if (data % 2 == 0 && data > 0)
+    {
+        console.log(data+ " is a even number");
+
+    }
+    else
+    {
+        console.log("Please enter a valid number");
+    }
+        
+}
+condition()
---
+++ for_loop.js
@@ -0,0 +1,17 @@
+
+function loop()
+{
+  const data_four = ["apple",13,"piash",20];
+//   let i =0;
+//   while(i<data_four.length)
+//   {
+//     console.log(data_four[i]);
+//     i++
+//   }
+
+    for(let i=0;i<data_four.length;i++)
+    {
+        console.log(data_four[i]);
+    }
+}
+loop()
